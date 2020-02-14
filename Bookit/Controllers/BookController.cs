27ab72654ba0774ca537586ff3e97b5bda4f4724
Ali <r.barzegarr@gmail.com>
2020-02-14 22:59:23 +0330

    Books Create

---
+++ Bookit/Controllers/BookController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookit.Data;
+using Bookit.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookit.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _db.Books;
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book model)
+        {
+         
+            _db.Books.Add(model);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
